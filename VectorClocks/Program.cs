73c8ae53da2cf2ea3b72547363f79a149de19316
Bartosz Sypytkowski <b.sypytkowski@gmail.com>
2018-08-17 18:29:34 +0100

    Add project files.

---
+++ VectorClocks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VectorClocks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
